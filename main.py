c9eec1a46d632d25020af0111d98af1c761067a7
Nur Jamiludin Ramadhan <j.maill.mail@gmail.com>
2023-10-10 08:18:04 +0700

    update bending algorithm and operation screen

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ P_GAIN = 1.0
 # PIN_FWD = 16
 # PIN_REV = 18
 
-USERNAME = 'labtek'
+USERNAME = 'pipe_bending_cnc'
 # DISK_ADDRESS = Path("/media/pi/RESDONGLE/")
 DISK_ADDRESS = Path("/media/" + USERNAME + "/RESDONGLE/")
 SERIAL_NUMBER = "2301212112233412"
@@ -240,8 +240,8 @@ class ScreenPipeSetting(BoxLayout):
     def screen_advanced_setting(self):
         self.screen_manager.current = 'screen_advanced_setting'
 
-    def screen_operate(self):
-        self.screen_manager.current = 'screen_operate'
+    def screen_operate_auto(self):
+        self.screen_manager.current = 'screen_operate_auto'
 
     def screen_compile(self):
         self.screen_manager.current = 'screen_compile'
@@ -287,8 +287,8 @@ class ScreenMachineSetting(BoxLayout):
     def screen_advanced_setting(self):
         self.screen_manager.current = 'screen_advanced_setting'
 
-    def screen_operate(self):
-        self.screen_manager.current = 'screen_operate'
+    def screen_operate_auto(self):
+        self.screen_manager.current = 'screen_operate_auto'
 
     def screen_compile(self):
         self.screen_manager.current = 'screen_compile'
@@ -313,8 +313,8 @@ class ScreenAdvancedSetting(BoxLayout):
     def screen_advanced_setting(self):
         self.screen_manager.current = 'screen_advanced_setting'
 
-    def screen_operate(self):
-        self.screen_manager.current = 'screen_operate'
+    def screen_operate_auto(self):
+        self.screen_manager.current = 'screen_operate_auto'
 
     def screen_compile(self):
         self.screen_manager.current = 'screen_compile'
@@ -324,14 +324,43 @@ class ScreenAdvancedSetting(BoxLayout):
         toast("shutting down system")
         os.system("shutdown -h now")
 
-class ScreenOperate(BoxLayout):
+class ScreenOperateManual(BoxLayout):
+    screen_manager = ObjectProperty(None)
+
+    def __init__(self, **kwargs):      
+        super(ScreenOperateManual, self).__init__(**kwargs)
+
+    def screen_pipe_setting(self):
+        self.screen_manager.current = 'screen_pipe_setting'
+
+    def screen_machine_setting(self):
+        self.screen_manager.current = 'screen_machine_setting'
+
+    def screen_advanced_setting(self):
+        self.screen_manager.current = 'screen_advanced_setting'
+
+    def screen_operate_manual(self):
+        self.screen_manager.current = 'screen_operate_manual'
+
+    def screen_operate_auto(self):
+        self.screen_manager.current = 'screen_operate_auto'
+
+    def screen_compile(self):
+        self.screen_manager.current = 'screen_compile'
+
+    def exec_shutdown(self):
+        # os.system("shutdown /s /t 1") #for windows os
+        toast("shutting down system")
+        os.system("shutdown -h now")
+
+class ScreenOperateAuto(BoxLayout):
     screen_manager = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         global dt_time
         global dt_cycle
         
-        super(ScreenOperate, self).__init__(**kwargs)
+        super(ScreenOperateAuto, self).__init__(**kwargs)
         Clock.schedule_once(self.delayed_init)
         self.file_manager = MDFileManager(
             exit_manager=self.exit_manager, select_path=self.select_path
@@ -366,6 +395,7 @@ class ScreenOperate(BoxLayout):
             if self.manager_open:
                 self.file_manager.back()
         return True
+    
     def delayed_init(self, dt):
         global pipe_diameter
         global machine_die_radius
@@ -374,47 +404,35 @@ class ScreenOperate(BoxLayout):
         global step_bend
         global step_turn
 
-        step_length[0] = 100.
-        step_bend[0] = 90.
-        step_turn[0] = 0.
-
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111, projection='3d')
         self.fig.set_facecolor("#eeeeee")
         self.fig.tight_layout()
 
-        # straight pipe
-        Uc = np.linspace(0, 2 * np.pi, 32)
-        Xc = np.linspace(0, step_length[0], 4)
-        Uc, Xc = np.meshgrid(Uc, Xc)
+        offset_length = step_length
+        bend_angle = step_bend / 180 * np.pi
+        turn_angle = step_turn / 180 * np.pi
         pipe_radius = pipe_diameter / 2
-        Yc = pipe_radius * np.cos(Uc)
-        Zc = pipe_radius * np.sin(Uc)
 
-        # theta: poloidal angle; phi: toroidal angle 
-        bend_angle = step_bend[0] / 180 * np.pi
-        theta = np.linspace(0, 2*np.pi, 100) 
-        phi   = np.linspace(0, bend_angle, 100) 
-        theta, phi = np.meshgrid(theta, phi) 
+        Uo = np.linspace(0, 2 * np.pi, 30)
+        Yo = np.linspace(0, 0, 5)
+        Uo, Yo = np.meshgrid(Uo, Yo)
+        Xo = pipe_radius * np.cos(Uo) - machine_die_radius
+        Zo = pipe_radius * np.sin(Uo)
 
-        # R0: major radius; a: minor radius 
-        R0, a = machine_die_radius, pipe_radius 
+        screen_compile = ScreenCompile()
+        
+        X0, Y0, Z0 = screen_compile.simulate(Xo, Yo, Zo, offset_length[0], bend_angle[0], turn_angle[0])
 
-        # torus parametrization 
-        y = (R0 + a*np.cos(theta)) * np.cos(phi) - machine_die_radius
-        x = (R0 + a*np.cos(theta)) * np.sin(phi) + step_length[0]
-        z = a * np.sin(theta) 
-
-        self.ax.plot_surface(Xc, Yc, Zc, color='gray')
-        self.ax.plot_surface(x, y, z, color='gray')
+        self.ax.plot_surface(X0, Y0, Z0, color='gray', alpha=1)
         # self.ax.set_box_aspect(aspect=(1, 1, 1))
         self.ax.set_aspect('equal')
         # self.ax.set_xlim([0, 6000])
         # self.ax.set_ylim([-100, 100])
         # self.ax.set_zlim([-100, 100])
         # self.ax.axis('off')
-
         self.ids.pipe_bended_illustration.add_widget(FigureCanvasKivyAgg(self.fig))    
+   
 
     def screen_pipe_setting(self):
         self.screen_manager.current = 'screen_pipe_setting'
@@ -425,8 +443,11 @@ class ScreenOperate(BoxLayout):
     def screen_advanced_setting(self):
         self.screen_manager.current = 'screen_advanced_setting'
 
-    def screen_operate(self):
-        self.screen_manager.current = 'screen_operate'
+    def screen_operate_manual(self):
+        self.screen_manager.current = 'screen_operate_manual'
+
+    def screen_operate_auto(self):
+        self.screen_manager.current = 'screen_operate_auto'
 
     def screen_compile(self):
         self.screen_manager.current = 'screen_compile'
@@ -439,66 +460,22 @@ class ScreenOperate(BoxLayout):
 class ScreenCompile(BoxLayout):
     screen_manager = ObjectProperty(None)
     global flag_run
+    global step_length
+    global step_bend
+    global step_turn
+
+    global pipe_diameter
+    global machine_die_radius
 
     def __init__(self, **kwargs):
         super(ScreenCompile, self).__init__(**kwargs)
         Clock.schedule_once(self.delayed_init)
 
     def delayed_init(self, dt):
-        global pipe_diameter
-        global machine_die_radius
-
-        global step_length
-        global step_bend
-        global step_turn
-
-        # step_length[0] = 100.
-        # step_bend[0] = 90.
-        # step_turn[0] = 0.
-
-        self.ids.input_step_length0.text = str(step_length[0])
-        self.ids.input_step_bend0.text = str(step_bend[0])
-        self.ids.input_step_turn0.text = str(step_turn[0])
-
-        self.ids.input_step_length1.text = str(step_length[1])
-        self.ids.input_step_bend1.text = str(step_bend[1])
-        self.ids.input_step_turn1.text = str(step_turn[1])
-
-        self.ids.input_step_length2.text = str(step_length[2])
-        self.ids.input_step_bend2.text = str(step_bend[2])
-        self.ids.input_step_turn2.text = str(step_turn[2])
-
-        self.ids.input_step_length3.text = str(step_length[3])
-        self.ids.input_step_bend3.text = str(step_bend[3])
-        self.ids.input_step_turn3.text = str(step_turn[3])
-
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111, projection='3d')
-        self.fig.set_facecolor("#eeeeee")
-        self.fig.tight_layout()
-
-        offset_length = step_length
-        bend_angle = step_bend / 180 * np.pi
-        turn_angle = step_turn / 180 * np.pi
-        
-        X0, Y0, Z0 = self.simulate(offset_length[0], bend_angle[0], turn_angle[0])
-
-        self.ax.plot_surface(X0, Y0, Z0, color='red', alpha=0.5)
-        # self.ax.set_box_aspect(aspect=(1, 1, 1))
-        self.ax.set_aspect('equal')
-        # self.ax.set_xlim([0, 6000])
-        # self.ax.set_ylim([-100, 100])
-        # self.ax.set_zlim([-100, 100])
-        # self.ax.axis('off')
-        self.ids.pipe_bended_illustration.add_widget(FigureCanvasKivyAgg(self.fig))    
+        self.reset()
 
     def update(self):
-        global step_length
-        global step_bend
-        global step_turn
-        
         try:
-            self.ids.pipe_bended_illustration.clear_widgets()
             step_length[0] = float(self.ids.input_step_length0.text)
             step_bend[0] = float(self.ids.input_step_bend0.text)
             step_turn[0] = float(self.ids.input_step_turn0.text)
@@ -515,6 +492,32 @@ class ScreenCompile(BoxLayout):
             step_bend[3] = float(self.ids.input_step_bend3.text)
             step_turn[3] = float(self.ids.input_step_turn3.text)
 
+            step_length[4] = float(self.ids.input_step_length4.text)
+            step_bend[4] = float(self.ids.input_step_bend4.text)
+            step_turn[4] = float(self.ids.input_step_turn4.text)
+
+            step_length[5] = float(self.ids.input_step_length5.text)
+            step_bend[5] = float(self.ids.input_step_bend5.text)
+            step_turn[5] = float(self.ids.input_step_turn5.text)
+
+            step_length[6] = float(self.ids.input_step_length6.text)
+            step_bend[6] = float(self.ids.input_step_bend6.text)
+            step_turn[6] = float(self.ids.input_step_turn6.text)
+
+            step_length[7] = float(self.ids.input_step_length7.text)
+            step_bend[7] = float(self.ids.input_step_bend7.text)
+            step_turn[7] = float(self.ids.input_step_turn7.text)
+
+            step_length[8] = float(self.ids.input_step_length8.text)
+            step_bend[8] = float(self.ids.input_step_bend8.text)
+            step_turn[8] = float(self.ids.input_step_turn8.text)
+
+            step_length[9] = float(self.ids.input_step_length9.text)
+            step_bend[9] = float(self.ids.input_step_bend9.text)
+            step_turn[9] = float(self.ids.input_step_turn9.text)
+
+            self.ids.pipe_bended_illustration.clear_widgets()
+
             self.fig = plt.figure()
             self.ax = self.fig.add_subplot(111, projection='3d')
             self.fig.set_facecolor("#eeeeee")
@@ -523,16 +526,26 @@ class ScreenCompile(BoxLayout):
             offset_length = step_length
             bend_angle = step_bend / 180 * np.pi
             turn_angle = step_turn / 180 * np.pi
-            
-            X0, Y0, Z0 = self.simulate(offset_length[0], bend_angle[0], turn_angle[0])
-            X1, Y1, Z1 = self.simulate(offset_length[1], bend_angle[1], turn_angle[1])
-            X2, Y2, Z2 = self.simulate(offset_length[2], bend_angle[2], turn_angle[2])
-            X3, Y3, Z3 = self.simulate(offset_length[3], bend_angle[3], turn_angle[3])
+            pipe_radius = pipe_diameter / 2
 
-            self.ax.plot_surface(X0, Y0, Z0, color='red', alpha=0.5)
-            self.ax.plot_surface(X1, Y1, Z1, color='green', alpha=0.5)
-            self.ax.plot_surface(X2, Y2, Z2, color='blue', alpha=0.5)
-            self.ax.plot_surface(X3, Y3, Z3, color='gray', alpha=0.5)
+            Uo = np.linspace(0, 2 * np.pi, 30)
+            Yo = np.linspace(0, 0, 5)
+            Uo, Yo = np.meshgrid(Uo, Yo)
+            Xo = pipe_radius * np.cos(Uo) - machine_die_radius
+            Zo = pipe_radius * np.sin(Uo)
+            
+            X0, Y0, Z0 = self.simulate(Xo, Yo, Zo, offset_length[0], bend_angle[0], turn_angle[0])
+            X1, Y1, Z1 = self.simulate(X0, Y0, Z0, offset_length[1], bend_angle[1], turn_angle[1])
+            X2, Y2, Z2 = self.simulate(X1, Y1, Z1, offset_length[2], bend_angle[2], turn_angle[2])
+            X3, Y3, Z3 = self.simulate(X2, Y2, Z2, offset_length[3], bend_angle[3], turn_angle[3])
+            X4, Y4, Z4 = self.simulate(X3, Y3, Z3, offset_length[4], bend_angle[4], turn_angle[4])
+            X5, Y5, Z5 = self.simulate(X4, Y4, Z4, offset_length[5], bend_angle[5], turn_angle[5])
+            X6, Y6, Z6 = self.simulate(X5, Y5, Z5, offset_length[6], bend_angle[6], turn_angle[6])
+            X7, Y7, Z7 = self.simulate(X6, Y6, Z6, offset_length[7], bend_angle[7], turn_angle[7])
+            X8, Y8, Z8 = self.simulate(X7, Y7, Z7, offset_length[8], bend_angle[8], turn_angle[8])
+            X9, Y9, Z9 = self.simulate(X8, Y8, Z8, offset_length[9], bend_angle[9], turn_angle[9])
+
+            self.ax.plot_surface(X9, Y9, Z9, color='gray', alpha=1)
             # self.ax.set_box_aspect(aspect=(1, 1, 1))
             self.ax.set_aspect('equal')
             # self.ax.set_xlim([0, 6000])
@@ -543,24 +556,22 @@ class ScreenCompile(BoxLayout):
         except:
             toast("error update pipe illustration")
 
-    def simulate(self, offset_length, bend_angle, turn_angle):
-        global pipe_diameter
-        global machine_die_radius
-
+    def simulate(self, prev_X, prev_Y, prev_Z, offset_length, bend_angle, turn_angle):
         pipe_radius = pipe_diameter / 2
         # step 1 : create straight pipe
         # straight pipe
         Ua = np.linspace(0, 2 * np.pi, 30)
-        Ya = np.linspace(0, offset_length, 5)
+        Ya = np.linspace(offset_length, 0, 5)
         Ua, Ya = np.meshgrid(Ua, Ya)
-        
         Xa = pipe_radius * np.cos(Ua) - machine_die_radius
-        Ya = -Ya + offset_length
         Za = pipe_radius * np.sin(Ua)
+        # combine become one object with previous mesh
+        Xa = np.append(prev_X, Xa, axis=0)
+        Ya = np.append(prev_Y + offset_length, Ya, axis=0)
+        Za = np.append(prev_Z, Za, axis=0)
 
         # step 2 : create bended pipe
         # theta: poloidal angle; phi: toroidal angle 
-        
         theta = np.linspace(0, 2 * np.pi, 30) 
         phi   = np.linspace(0, bend_angle, 30) 
         theta, phi = np.meshgrid(theta, phi) 
@@ -592,7 +603,54 @@ class ScreenCompile(BoxLayout):
 
         return Xf, Yf, Zf
 
-    def save_graph(self):
+    def reset(self):
+        step_length = np.zeros(10)
+        step_bend = np.zeros(10)
+        step_turn = np.zeros(10)
+
+        self.ids.input_step_length0.text = str(step_length[0])
+        self.ids.input_step_bend0.text = str(step_bend[0])
+        self.ids.input_step_turn0.text = str(step_turn[0])
+
+        self.ids.input_step_length1.text = str(step_length[1])
+        self.ids.input_step_bend1.text = str(step_bend[1])
+        self.ids.input_step_turn1.text = str(step_turn[1])
+
+        self.ids.input_step_length2.text = str(step_length[2])
+        self.ids.input_step_bend2.text = str(step_bend[2])
+        self.ids.input_step_turn2.text = str(step_turn[2])
+
+        self.ids.input_step_length3.text = str(step_length[3])
+        self.ids.input_step_bend3.text = str(step_bend[3])
+        self.ids.input_step_turn3.text = str(step_turn[3])
+
+        self.ids.input_step_length4.text = str(step_length[4])
+        self.ids.input_step_bend4.text = str(step_bend[4])
+        self.ids.input_step_turn4.text = str(step_turn[4])
+
+        self.ids.input_step_length5.text = str(step_length[5])
+        self.ids.input_step_bend5.text = str(step_bend[5])
+        self.ids.input_step_turn5.text = str(step_turn[5])
+
+        self.ids.input_step_length6.text = str(step_length[6])
+        self.ids.input_step_bend6.text = str(step_bend[6])
+        self.ids.input_step_turn6.text = str(step_turn[6])
+
+        self.ids.input_step_length7.text = str(step_length[7])
+        self.ids.input_step_bend7.text = str(step_bend[7])
+        self.ids.input_step_turn7.text = str(step_turn[7])
+
+        self.ids.input_step_length8.text = str(step_length[8])
+        self.ids.input_step_bend8.text = str(step_bend[8])
+        self.ids.input_step_turn8.text = str(step_turn[8])
+
+        self.ids.input_step_length9.text = str(step_length[9])
+        self.ids.input_step_bend9.text = str(step_bend[9])
+        self.ids.input_step_turn9.text = str(step_turn[9]) 
+
+        self.update()
+
+    def save(self):
         try:
             now = datetime.now().strftime("/%d_%m_%Y_%H_%M_%S.jpg")
             disk = str(DISK_ADDRESS) + now
@@ -603,18 +661,6 @@ class ScreenCompile(BoxLayout):
             print("error saving graph")
             toast("error saving graph")
                 
-    def autosave_graph(self):
-        try:
-            now = datetime.now().strftime("/%d_%m_%Y_%H_%M_%S.jpg")
-            cwd = os.getcwd()
-            disk = cwd + now
-            self.fig.savefig(disk)
-            print("sucessfully auto save graph")
-            # toast("sucessfully save graph")
-        except:
-            print("error auto saving graph")
-            # toast("error saving graph")
-                
     def screen_pipe_setting(self):
         self.screen_manager.current = 'screen_pipe_setting'
 
@@ -624,8 +670,8 @@ class ScreenCompile(BoxLayout):
     def screen_advanced_setting(self):
         self.screen_manager.current = 'screen_advanced_setting'
 
-    def screen_operate(self):
-        self.screen_manager.current = 'screen_operate'
+    def screen_operate_auto(self):
+        self.screen_manager.current = 'screen_operate_auto'
 
     def screen_compile(self):
         self.screen_manager.current = 'screen_compile'
