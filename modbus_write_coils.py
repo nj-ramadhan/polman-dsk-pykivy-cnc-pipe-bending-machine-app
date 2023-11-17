f50f5f69aa0342acdf3a5ca22d0c374c5c3bcc9c
Nur Jamiludin Ramadhan <j.maill.mail@gmail.com>
2023-11-17 18:10:04 +0700

    new file:   modbus_write_coils.py 	new file:   mqtt_pub.py 	new file:   mqtt_publish.py 	new file:   mqtt_subscribe.py

---
+++ modbus_write_coils.py
@@ -0,0 +1,23 @@
+from pymodbus.client import ModbusSerialClient
+client = ModbusSerialClient('COM9', baudrate=19200, bytesize=8, parity="N", stopbits=2, timeout=1, strict=False)
+
+# discrete_inputs = client.read_discrete_inputs(1, 14)   # start reading from address 0 
+# discrete_inputs.setBit(3, 1)                       # set address 3 to value 1 
+# discrete_inputs.setBit(1,1)
+# print(discrete_inputs.getBit(0))
+# print(discrete_inputs.getBit(1))
+# print(discrete_inputs.bits[:])
+# print()
+
+client.write_coils(1536, [True, True, True, True, True, True, True, True])
+# client.write_coil(1536, True)
+# client.write_coil(1537, True)
+# client.write_coil(1538, True)
+
+client.write_coils(1536, [False, False, False, False, True, True, True, True])
+client.write_coils(1536, [False, False, False, False, False, False, False, False])
+# client.write_coil(1536, False)
+# client.write_coil(1537, False)
+# client.write_coil(1538, False)
+# reading_coil = client.read_coils(0, 14)  # start reading from address 1
+# print(reading_coil.bits[:])
---
+++ mqtt_pub.py
@@ -0,0 +1,24 @@
+import paho.mqtt.publish as publish
+import time
+
+# msgs = [{'topic':"data_out/test", 'payload':200},
+#         ("paho/test/multiple", "multiple 2", 0, False)]
+# publish.multiple("data_out/test", 201, hostname="broker.hivemq.com", port=1883)
+
+n = 1
+good = 1
+not_good = 1
+
+while(good < 100):
+    msgs = [{'topic':"data_out/test", 'payload':good},
+        {'topic':"data_out/test", 'payload':not_good},]
+
+    publish.multiple(msgs, hostname="broker.hivemq.com", port=1883)
+    print("n:" + str(n))
+    time.sleep(0.5)
+
+    n+=1
+    good+=1
+    not_good+=1
+
+
